d5a86053743f875028449de6f95820bc4b9a0590
sSkittleSs <anton.bril.blr@gmail.com>
2020-09-16 08:27:45 +0300

    Обновлен первоначальный дизайн окна

--- SDT-Project/MainWindow.xaml.cs
+++ SDT-Project/MainWindow.xaml.cs
@@ -24,5 +24,59 @@ namespace SDT_Project
         {
             InitializeComponent();
         }
+
+        private void EllipseExit_MouseEnter(object sender, MouseEventArgs e)
+        {
+            EllipseExit.Fill = Brushes.DarkRed;
+            Mouse.OverrideCursor = Cursors.Hand;
+        }
+
+        private void EllipseExit_MouseLeave(object sender, MouseEventArgs e)
+        {
+            EllipseExit.Fill = Brushes.Red;
+            Mouse.OverrideCursor = Cursors.Arrow;
+        }
+
+        private void EllipseMaximize_MouseEnter(object sender, MouseEventArgs e)
+        {
+            EllipseMaximize.Fill = Brushes.Gray;
+            Mouse.OverrideCursor = Cursors.Hand;
+        }
+
+        private void EllipseMaximize_MouseLeave(object sender, MouseEventArgs e)
+        {
+            EllipseMaximize.Fill = Brushes.LightSlateGray;
+            Mouse.OverrideCursor = Cursors.Arrow;
+        }
+
+        private void EllipseMinimize_MouseEnter(object sender, MouseEventArgs e)
+        {
+            EllipseMinimize.Fill = Brushes.LightGray;
+            Mouse.OverrideCursor = Cursors.Hand;
+        }
+
+        private void EllipseMinimize_MouseLeave(object sender, MouseEventArgs e)
+        {
+            EllipseMinimize.Fill = Brushes.White;
+            Mouse.OverrideCursor = Cursors.Arrow;
+        }
+
+        private void EllipseExit_Click(object sender, MouseEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void EllipseMaximize_Click(object sender, MouseEventArgs e)
+        {
+            WindowState = WindowState.Maximized;
+        }
+        private void EllipseMinimize_Click(object sender, MouseEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void FreeNotify_Click(object sender, MouseEventArgs e) // TODO: Удалить после реализации изменения размера окна.
+        {
+            WindowState = WindowState.Normal;
+        }
     }
 }
